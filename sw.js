3083fa9768962eca6808334847103101af765c82
jsn-b <mailtojsnb@gmail.com>
2024-09-01 08:21:42 +0530

    Create serviceworker.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './styles.css',
+        './script.js',
+        './manifest.json',
+        'https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js',
+        './font.woff2'
+        // Add other assets you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

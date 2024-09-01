211a549c32c06a9f988a0de3805c27bf1cfda6f5
jsn-b <mailtojsnb@gmail.com>
2024-09-01 08:22:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -185,3 +185,12 @@ function finishTable() {
     let fileName = "Pathra " + new Date().toISOString().split('T')[0] + ".pdf";
     doc.save(fileName);
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(error) {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
